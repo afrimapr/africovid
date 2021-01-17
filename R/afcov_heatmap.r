751f062ade7450811574fd9fc536219be02222ef
AndySouth <southandy@gmail.com>
2021-01-17 17:49:36 +0000

    ggplot dependency

--- R/afcov_heatmap.r
+++ R/afcov_heatmap.r
@@ -13,6 +13,8 @@
 # afcov_heatmap('Mali')
 #'
 #' @return \code{ggplot}
+#' @importFrom ggplot2 ggplot aes_string geom_tile theme_classic labs scale_fill_distiller scale_x_date theme element_blank
+#' @importFrom lubridate parse_date_time
 #' @export
 #'
 #'
